6791ec2a46e3884bbfc2789b9cd7522f4e1ac844
jvalde17 <jvalde17@asu.edu>
2018-11-15 19:44:35 -0800

    Initial java project creation

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Jess Valdez
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to my GOP patterns application.");
+
+    }
+
+}
