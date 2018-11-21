042b74897b684f4ab2fb2395c9e9f201e36e4531
jvalde17 <jvalde17@asu.edu>
2018-11-21 10:39:47 -0800

    Added Apiary.java

---
+++ src/Apiary.java
@@ -0,0 +1,27 @@
+/**
+ * a singleton apiary class implementation
+ * 
+ * @author Jess Valdez
+ *
+ */
+public class Apiary {
+    private static Apiary instance = null; 
+    
+    // attributes of an apiary
+    public String name; 
+
+    // private constructor restricted to this class itself 
+    private Apiary() { 
+        this.name = "Cordova Apiary";
+    } 
+  
+    // static method to create instance of Apiary
+    public static Apiary testInstance() 
+    { 
+        if (instance == null) {
+            instance = new Apiary(); 
+        }
+  
+        return instance; 
+    } 
+}
